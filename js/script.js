79c48a4ba31f005f30a5dff82f97bd89c3baa665
ivanvit100 <ivanvit100@gmail.com>
2022-04-11 17:37:39 +0300

    semantics improvement

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 //Регистрация service-worker
-
 if ('serviceWorker' in navigator){
  	navigator.serviceWorker.register('/service.js', {
     		scope: '/'
   	});
 }
 
-
 document.addEventListener("DOMContentLoaded", function(event){ 
 	//Анимации
 	new WOW().init();	
 
 	//Прелоадер
-	setTimeout(function() { preloader.seen = false; }, 1000);
+	setTimeout(function(){ 
+    preloader.seen = false; 
+  }, 1000);
 
 	//Шапка сайта
 	window.addEventListener('scroll', function(e){
@@ -23,56 +23,39 @@ document.addEventListener("DOMContentLoaded", function(event){
 		header_bg.style.filter = "blur(" + (scrl/200) + "px)";
 	});
 
-      	//3D
-      	var paste = document.getElementById("about_model");
-      	let scene, camera, renderer;
-    	function init(){
-      		scene = new THREE.Scene();
-      		scene.background = new THREE.Color(0xFFFFFF);
-        		camera = new THREE.PerspectiveCamera(45, 421 / 698, 0.1, 1000);
-      		camera.position.z = 150;
-      		hlight = new THREE.AmbientLight(0x404040, 10);
-      		scene.add(hlight);
-      		renderer = new THREE.WebGLRenderer({antialias:true});
-        		renderer.setSize(421, 698);
-      		if(window.screen.width > 480){
-      			paste.appendChild(renderer.domElement);
-      		}
-      		let loader = new THREE.GLTFLoader();
-        		loader.load('/model/scene.gltf', function(gltf){
-        			obj = gltf.scene.children[0];
-        			obj.scale.set(1.3, 1.3, 1.3);
-       			scene.add(gltf.scene);
-       			animate();
-          		});
-      	}
-      	function animate(){
-   		requestAnimationFrame(animate);
-   		if(obj)
-    		obj.rotation.z += 0.003;
-       		renderer.render(scene,camera);
-      	}
-    	init();
+  //3D
+  var paste = document.getElementById("about_model");
+  let scene, camera, renderer;
+  function init(){
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xFFFFFF);
+    camera = new THREE.PerspectiveCamera(45, 421 / 698, 0.1, 1000);
+    camera.position.z = 150;
+    hlight = new THREE.AmbientLight(0x404040, 10);
+    scene.add(hlight);
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setSize(421, 698);
+    if(window.screen.width > 480){
+      paste.appendChild(renderer.domElement);
+    }
+    let loader = new THREE.GLTFLoader();
+    loader.load('./model/scene.gltf', function(gltf){
+      obj = gltf.scene.children[0];
+      obj.scale.set(1.3, 1.3, 1.3);
+      scene.add(gltf.scene);
+      animate();
+    });
+  }
+  function animate(){
+    requestAnimationFrame(animate);
+   	if(obj)
+    	obj.rotation.z += 0.003;
+    renderer.render(scene,camera);
+  }
+  init();
 
-    	//Анимация появления 3D
-    	cnvs = document.querySelector("canvas");
-    	cnvs.classList.add("wow");
-    	cnvs.classList.add("fadeInRight");
-
-	//Установка PWA
-	let deferredPrompt;
-	const addBtn = document.querySelector('#add_button');
-	addBtn.style.display = 'none';
-	window.addEventListener('beforeinstallprompt', (e) => {
-		e.preventDefault();
-		deferredPrompt = e;
-		addBtn.style.display = 'block';
-		addBtn.addEventListener('click', (e) => {
-			addBtn.style.display = 'none';
-			deferredPrompt.prompt();
-			deferredPrompt.userChoice.then((choiceResult) => {
-				deferredPrompt = null;
-			});
-		});
-	});
+  //Анимация появления 3D
+  cnvs = document.querySelector("canvas");
+  cnvs.classList.add("wow");
+  cnvs.classList.add("fadeInRight");
 }); 
