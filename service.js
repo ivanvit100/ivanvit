852dabd58e9ecf1bd98896c6db5d406d70d79e24
ivanvit100 <ivanvit100@gmail.com>
2022-06-03 21:04:56 +0300

    Footer grid

--- service.js
+++ service.js
@@ -1,5 +1,5 @@
-var CACHE_STATIC_NAME = 'static-v49';
-var CACHE_DYNAMIC_NAME = 'dynamic-v49';
+var CACHE_STATIC_NAME = 'static-v50';
+var CACHE_DYNAMIC_NAME = 'dynamic-v50';
 
 self.addEventListener('install', function (event) {
   console.log('Installing Service Worker ...');
